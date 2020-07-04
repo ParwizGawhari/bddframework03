f60296b8f7c1299f9a7b3d68ea560134eb034d83
ParwizGawhari <gawhariparwiz@gmail.com>
2020-07-04 06:16:59 -0700

    test sdet

---
+++ src/test/java/us/tecschool/testNG/sdet02/testNG/sdet02/AppTest.java
@@ -0,0 +1,20 @@
+package us.tecschool.testNG.sdet02.testNG.sdet02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
